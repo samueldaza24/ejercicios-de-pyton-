86ca484e92c26a4996ee41b3ba37784f7feedfc8
samueldaza24 <samidaza24@gmail.com>
2026-07-26 14:53:20 -0500

    Add files via upload

---
+++ samueldaza7.py
@@ -0,0 +1,6 @@
+numero1=int(input("ingrese el primer numero"))
+numero2=int(input(" ingrese el segundo numero"))
+if numero1>numero2:
+    print(" el numero 1 es mayor")
+else:
+    print(" el numero 2 es mayor")
