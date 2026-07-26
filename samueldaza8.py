c4f4adfbef39718f64897100962d9e9daa8191ce
samueldaza24 <samidaza24@gmail.com>
2026-07-26 14:53:33 -0500

    Add files via upload

---
+++ samueldaza8.py
@@ -0,0 +1,8 @@
+numero1=int(input(" ingrese el primer numero"))
+numero2=int(input(" ingrese el segundo numro"))
+if numero1> numero2:
+    print(" el primer numero es mayor")
+elif numero2>numero1:
+    print(" el segundo numero es mayor")
+else:
+    print(" los dos numeros son iguales")
